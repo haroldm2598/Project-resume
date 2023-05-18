5eb8f9f738827160ff01516ba9efab53dd1fd016
Harold <cvuyante24@gmail.com>
2023-05-18 21:00:35 +0800

    Add conditional rendering toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react';
-import Editor from './component/Editor';
+import Details from './component/Details';
+import Work from './component/Work';
 import Post from './component/Post';
 import './styles/main.scss';
 
 export default function App() {
 	const [form, setForm] = useState(setDetails());
+	const [work, setWork] = useState(setWorkDetails());
+	const [deleteWork, setDeleteWork] = useState(work[0].isShow);
+
+	function deleteToggle(event) {
+		event.preventDefault();
+		setDeleteWork((prevShown) => !prevShown);
+	}
 
 	function setDetails() {
 		const details = {
@@ -19,7 +27,22 @@ export default function App() {
 		return details;
 	}
 
-	function handleChange(event) {
+	function setWorkDetails() {
+		const details = [
+			{
+				companyName: 'A Software Engineer',
+				workPosition: 'Software Engineer',
+				dateStart: '2018',
+				dateEnd: '2020',
+				workDetails: 'france, milan',
+				isShow: true
+			}
+		];
+
+		return details;
+	}
+
+	function handleChange() {
 		const { name, value } = event.target;
 
 		setForm((prevForm) => {
@@ -28,32 +51,49 @@ export default function App() {
 				[name]: value
 			};
 		});
+
+		setWork((prevForm) => {
+			return {
+				...prevForm,
+				[name]: value
+			};
+		});
+
+		console.log(work);
 	}
+
 	return (
 		<>
 			<div className='container'>
-				<Editor
-					title={'Personal Details'}
-					detail1={'full name'}
-					detail2={'position'}
-					detail3={'phone number'}
-					detail4={'email address'}
-					detail5={'location'}
-					detail6={'comments'}
-					name1={'fullName'}
-					name2={'position'}
-					name3={'phoneNumber'}
-					name4={'emailAddress'}
-					name5={'location'}
-					name6={'comments'}
-					form1={form.fullName}
-					form2={form.position}
-					form3={form.phoneNumber}
-					form4={form.emailAddress}
-					form5={form.location}
-					form6={form.comments}
-					handleChange={handleChange}
-				/>
+				<div className='detailsContainer'>
+					<Details
+						title={'Personal Details'}
+						name1={'fullName'}
+						name2={'position'}
+						name3={'phoneNumber'}
+						name4={'emailAddress'}
+						name5={'location'}
+						name6={'comments'}
+						form1={form.fullName}
+						form2={form.position}
+						form3={form.phoneNumber}
+						form4={form.emailAddress}
+						form5={form.location}
+						form6={form.comments}
+						handleChange={handleChange}
+					/>
+
+					<Work
+						work1={work.companyName}
+						work2={work.workPosition}
+						work3={work.dateStart}
+						work4={work.dateEnd}
+						work5={work.workDetails}
+						isShow={deleteWork}
+						deleteToggle={deleteToggle}
+						handleChange={handleChange}
+					/>
+				</div>
 				<Post
 					form1={form.fullName}
 					form2={form.position}
@@ -61,6 +101,12 @@ export default function App() {
 					form4={form.emailAddress}
 					form5={form.location}
 					form6={form.comments}
+					workTitle={'work experience'}
+					work1={work[0].workPosition}
+					work2={work[0].companyName}
+					work3={work[0].dateStart}
+					work4={work[0].dateEnd}
+					work5={work[0].workDetails}
 				/>
 			</div>
 		</>
---
+++ src/component/Details.jsx
@@ -0,0 +1,51 @@
+export default function Details(props) {
+	return (
+		<>
+			<h1>{props.title}</h1>
+			<form>
+				<input
+					type='text'
+					placeholder='full name'
+					onChange={props.handleChange}
+					name={props.name1}
+					value={props.form1}
+				></input>
+				<input
+					type='text'
+					placeholder='position'
+					onChange={props.handleChange}
+					name={props.name2}
+					value={props.form2}
+				></input>
+				<input
+					type='text'
+					placeholder='phone number'
+					onChange={props.handleChange}
+					name={props.name3}
+					value={props.form3}
+				></input>
+				<input
+					type='email'
+					placeholder='email address'
+					onChange={props.handleChange}
+					name={props.name4}
+					value={props.form4}
+				></input>
+				<input
+					type='text'
+					placeholder='location'
+					onChange={props.handleChange}
+					name={props.name5}
+					value={props.form5}
+				></input>
+				<textarea
+					className='textArea'
+					placeholder='comments'
+					onChange={props.handleChange}
+					name={props.name6}
+					value={props.form6}
+				></textarea>
+			</form>
+		</>
+	);
+}
